2c92bf53417bb8eb6f6e7daea32218e764ee77c9
Sergio Varela <varela.a.sergio@gmail.com>
2022-12-09 13:06:23 -0700

    completed runtime

--- cs/runtime.js
+++ cs/runtime.js
@@ -47,16 +47,16 @@ const extraLargeArray = getSizedArray(100000);
 
 // Try it with first function
 perf.start();                     // Starts timer
-doublerAppend(extraLargeArray);
+doublerAppend(tinyArray);
 let resultsAppend = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
-doublerInsert(extraLargeArray);
+doublerInsert(tinyArray);
 let resultsInsert = perf.stop();
 
 
-console.log('Results for the extraLargeArray');
+console.log('Results for the tinyArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
